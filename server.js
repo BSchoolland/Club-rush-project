8ff019f709e9d225789ed74500d8a2cdff151aa6
OBenjOne <bschoolland@gmail.com>
2023-09-08 20:21:49 -0700

    fix: port and db

--- server.js
+++ server.js
@@ -1,31 +1,14 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Serve static files (like your HTML and JavaScript files)
 app.use(express.static('public'));
 // Parse incoming data
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// Connect to the MongoDB database
-mongoose.connect(process.env.DB_CONNECTION_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-
-db.on('error', (error) => {
-  console.error('Database connection error:', error);
-});
-
-db.once('open', () => {
-  console.log('Connected to the database');
-});
 
 // Start the server
 app.listen(port, () => {
